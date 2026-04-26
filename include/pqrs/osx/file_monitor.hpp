ada35c705d7dd50a996b905cbc805b322f61968f
Takayama Fumihiko <tekezo@pqrs.org>
2026-04-26 15:21:07 +0900

    Version 1.8.0

--- include/pqrs/osx/file_monitor.hpp
+++ include/pqrs/osx/file_monitor.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::file_monitor v1.7
+// pqrs::osx::file_monitor v1.8.0
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
